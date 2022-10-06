35c8d71445a892691ca201d58fb534d867de91be
onrdrmn <onrdrmn@gmail.com>
2022-10-07 02:19:09 +0300

    Lambda Implementation to Methods

--- DatabaseServices/Concrete/DbManagers/DbManagerBase.cs
+++ DatabaseServices/Concrete/DbManagers/DbManagerBase.cs
@@ -10,21 +10,10 @@ namespace DatabaseServices.Concrete.DatabaseManagers
         {
             _databaseService = databaseService;
         }
-        public void Create()
-        {
-            _databaseService.CreateData();
-        }
-        public void Read()
-        {
-            _databaseService.ReadData();
-        }
-        public void Update()
-        {
-            _databaseService.UpdateData();
-        }
-        public void Delete()
-        {
-            _databaseService.DeleteData();
-        }
+
+        public void Create() => _databaseService.CreateData();
+        public void Read() => _databaseService.ReadData();
+        public void Update() => _databaseService.UpdateData();
+        public void Delete() => _databaseService.DeleteData();
     }
 }
